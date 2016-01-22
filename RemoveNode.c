25d5d30b5b040a93e549d6a73027d4386d76bc56
EmirKalaci <emirkalaci@hotmail.com>
2016-01-22 03:41:55 +0200

    Comment style

--- RemoveNode.c
+++ RemoveNode.c
@@ -46,9 +46,9 @@ int main()
 
 
 /*
- *This function is to add a node to linked list.
- *It takes newValue as input and doesn't return a value as output.
- *At the same time it changes the value of head.
+ * This function is to add a node to linked list.
+ * It takes newValue as input and doesn't return a value as output.
+ * At the same time it changes the value of head.
  */
 void AddNodes(int newValue)
 {
@@ -59,11 +59,11 @@ void AddNodes(int newValue)
 	newNode -> value = newValue;
 
 	/*
-	 *If there is not any value in the linked list
-	 *newNode assigns as head of linked list
-	 *and null assigns to next of head.
-	 *else there is any value in the linked list
-	 *newNode assigns as head of linked list.
+	 * If there is not any value in the linked list
+	 * newNode assigns as head of linked list
+	 * and null assigns to next of head.
+	 * else there is any value in the linked list
+	 * newNode assigns as head of linked list.
 	 */
 	if(head == NULL)
 	{
@@ -80,9 +80,9 @@ void AddNodes(int newValue)
 
 
 /*
- *This function is to show values in the Linked List.
- *It doesn't take an input and doesn't return an output.
- *At the same time it doesn't change any global variable.
+ * This function is to show values in the Linked List.
+ * It doesn't take an input and doesn't return an output.
+ * At the same time it doesn't change any global variable.
  */
 void  display()
 {
@@ -105,9 +105,9 @@ void  display()
 
 
 /*
- *This function is to remove a value from linked list.
- *A variable is taken as an input and will be removed.
- *If remove variable is head , value of head changes as a global variable.
+ * This function is to remove a value from linked list.
+ * A variable is taken as an input and will be removed.
+ * If remove variable is head , value of head changes as a global variable.
  */
 void removeNode(int remove)
 {
@@ -119,16 +119,16 @@ void removeNode(int remove)
 	while(current != NULL)
 	{
 		/*
-		 *If remove value is found in the linked list
-		 *else go to the next value in the linked list.
+		 * If remove value is found in the linked list
+		 * else go to the next value in the linked list.
 		 */
 		if(current -> value == remove)
 		{
 			bool = true;
 
 			/*
-			 *If previous is null , value to be removed on head of
-			 *linked list. Else value to be removed is out of head.
+			 * If previous is null , value to be removed on head of
+			 * linked list. Else value to be removed is out of head.
 			 */
 			if(previous == NULL)
 			{
